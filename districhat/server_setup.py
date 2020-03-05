88e8c55d6b40664e1a507e9c49b8459bdef0bfa3
Toni Bomström <toni.bomstrom@gmail.com>
2020-03-05 14:26:18 +0200

    Add server setup script & rm old server on install
    Previously the new server was installed on top of the earlier one
    without first clearing the old one. Now the old one is first removed (if
    it is there to begin with) and the server is cloned afterwards.
    
    The server setup script is crucial as the setup service requires it to
    be present. On startup the setup script clones the repository and
    expects to be able to call the server setup script for completing the
    server installation.

---
+++ districhat/server_setup.py
@@ -0,0 +1,37 @@
+"""This setup script is used by the server upon startup to setup the serverside.
+"""
+import logging
+import os
+import subprocess
+
+SERVER_DIR = "/home/tonibom/DistriChat"
+SERVICE_FILE = "/services/districhat.service"
+SYSTEMD_SERVICE_LOCATION = "/etc/systemd/system/"
+
+_logger = logging.getLogger("SERVER-SETUP")
+
+
+def main():
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s:%(levelname)s: %(message)s")
+    _logger.info("Starting up...")
+    os.chdir(SERVER_DIR)
+
+    call = ["mv", SERVER_DIR + SERVICE_FILE, SYSTEMD_SERVICE_LOCATION]
+    subprocess.run(call, check=True)
+    _logger.info("Added the service file to services.")
+
+    call = ["sudo", "systemctl", "daemon-reload"]
+    subprocess.run(call, check=True)
+    _logger.info("Reloaded systemctl daemon.")
+
+    call = ["sudo", "systemctl", "start", "districhat.service"]
+    subprocess.run(call, check=True)
+    _logger.info("Started up districhat.service")
+
+    _logger.info("Finished server setup")
+
+
+if __name__ == "__main__":
+    main()
--- districhat/setup_service.py
+++ districhat/setup_service.py
@@ -7,8 +7,10 @@ import os
 import subprocess
 
 
-SERVER_PATH = "/home/tonibom"
 REPOSITORY = "git@github.com:tonibom/DistriChat.git"
+REPOSITORY_NAME = "DistriChat"
+SERVER_PATH = "/home/tonibom"
+SRC_LOCATION = "/districhat"
 
 _logger = logging.getLogger("DC-INSTALLER")
 
@@ -17,13 +19,18 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
 
     os.chdir(SERVER_PATH)
+    if os.path.exists(SERVER_PATH + "/" + REPOSITORY_NAME):
+        _logger.info("Repository exists already. Removing old one.")
+        call = ["rm", "-rf", REPOSITORY_NAME]
+        subprocess.run(call, check=True)
+        _logger.info("Old repository removed.")
 
     _logger.debug("Cloning repository {}...".format(REPOSITORY))
     call = ["git", "clone", REPOSITORY]
     subprocess.run(call, check=True)
     _logger.debug("Successfully cloned repository.")
 
-    os.chdir(SERVER_PATH + "/DistriChat")
+    os.chdir(SERVER_PATH + REPOSITORY_NAME + SRC_LOCATION)
 
     _logger.debug("Running server_setup.py")
     call = ["python3", "server_setup.py"]
